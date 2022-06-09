a0c6a5c274e02c9124df05f279200855471b3db1
Johannes Binder <j.binder.x@gmail.com>
2022-06-10 00:28:32 +0200

    Support Supergroup migrations
    # Conflicts:
    #	README.md
    #	init.example.py
    #	init.py

--- init.example.py
+++ init.example.py
@@ -19,6 +19,7 @@ from components.user.user_command_handler import UserCommandHandler
 from components.user.user_component import UserComponent
 from common.common_bot import CommonBot
 from common.services.telegram_service import TelegramService
+from components.user.user_migration_handler import UserMigrationHandler
 from components.user.user_service import UserService
 from texts import bot_name
 
@@ -26,7 +27,7 @@ from texts import bot_name
 def create_bot(admin_id: int):
     logger = get_logger()
     user_service = UserService()
-    telegram_service = TelegramService(user_service, logger)
+    telegram_service = TelegramService(user_service, logger, [UserMigrationHandler(), DoForMeMigrationHandler()])
     core_command_handler = CoreCommandHandler(admin_id, core_texts, telegram_service)
     user_command_handler = UserCommandHandler(admin_id, user_texts, telegram_service, bot_name, user_service)
     feedback_service = FeedbackService()
