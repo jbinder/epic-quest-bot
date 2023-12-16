4110b467bb7e7904f8d023e6e2b055509785d3ef
Johannes Binder <j.binder.x@gmail.com>
2023-12-17 00:57:33 +0100

    Add support for supergroups

--- components/quest/models.py
+++ components/quest/models.py
@@ -8,11 +8,11 @@ db = get_database()
 
 
 class Quest(db.Entity):
-    chat_id = Required(int)
-    created_by = Required(int)
+    chat_id = Required(int, size=64)
+    created_by = Required(int, size=64)
     created_at = Required(datetime)
     title = Required(str)
-    done_by = Optional(int)
+    done_by = Optional(int, size=64)
     done_at = Optional(datetime)
 
 
